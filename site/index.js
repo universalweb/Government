3ab8e2e80d2572d7687e4b4abf7287d6489c1f3b
sentivate <sentivate@protonmail.com>
2021-01-20 07:49:59 -0500

    End USDC and wBTC pools PROPOSAL

--- site/index.js
+++ site/index.js
@@ -23,6 +23,13 @@
 		title: `UW WEBSITE UPDATE`,
 		description: `First impressions count.Proposal to revamp universalweb.io, so that it breathes Bleeding Edge Tech like the UW itself does.No simple design, but something impressive for marketing the UW.`,
 		options: ['YES', 'NO']
+	}, {
+		id: '0xafcfbea6e669c080e3f2aaac505e1c57806a9c676640b6be0d00c3b5d79dfb22',
+		github: `https://github.com/universalweb/Government/discussions/28`,
+		title: `End USDC and wBTC pools`,
+		description: `This is a proposal to end the current USDC and wBTC pools effective immediately. Release the locked rewards over a 10 day period. We redistribute the leftover rewards to the DAI pool that will run until 28/2/2021.
+This will keep holders happy until wVIAT swap end offset potential dumping of the rewards from the other pools, as they can re-stake it in the DAI for a raised APY. Also encourages potential new investors to buy, as they can stake immediately all of their SNTVT until wVIAT is ready.`,
+		options: ['YES', 'NO']
 	}];
 	const ractive = window.Ractive({
 		data: {
